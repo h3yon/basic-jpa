a8d1803ddb89de7a6c72980096df20a5fa32cd4e
h3yon <khykhy1006@gmail.com>
2021-09-19 15:20:09 +0900

    feat: select Member

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -15,10 +16,18 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
+//            Member findMember = em.find(Member.class, 1L);
 
-            // persist로 저장 안 해도 됨
-            findMember.setName("HelloJPA");
+            // member 엔티티를 선택함 + pagination
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result){
+                System.out.println("member.name = " + member.getName());
+            }
+
 
             // JPA가 변경되었는지 커밋할 때쯤 체크
             tx.commit();
