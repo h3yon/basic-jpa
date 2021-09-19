11f8934c282ec961c55c9e310ff547a5281dfe99
h3yon <khykhy1006@gmail.com>
2021-09-19 18:40:44 +0900

    feat: ddl.auto

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,15 +3,23 @@ package hellojpa;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "MBR")
 public class Member {
 
     @Id
     private Long id;
+
+    // DDL 생성 기능. 실행 자체에 영향을 주진 않음
+    // JPA의 실행 로직에는 영향을 주지 않음
+    @Column(unique = true, length = 10)
     private String name;
+    private int age;
 
     public Long getId() {
         return id;
