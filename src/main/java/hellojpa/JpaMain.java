49fd160a0a5c51b2b755d5271efa0e69465b1c33
h3yon <khykhy1006@gmail.com>
2021-09-24 03:39:09 +0900

    feat: 불변객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -24,14 +24,19 @@ public class JpaMain {
             member.setHomeAddress(address);
             em.persist(member);
 
+            // 값 타입의 부작용 때문에
+            // 값 타입의 복사 필요
+            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
             Member member2 = new Member();
             member2.setUsername("member2");
-            member2.setHomeAddress(address);
+            // 기본 타입이 아니라 객체 타입이기 때문에
+            // 객체의 공유 참조는 피할 수 없다,,
+            // 그래서 setter 지우기
+            member2.setHomeAddress(copyAddress);
             em.persist(member2);
 
-            // 값 타입의 부작용
-            // member2도 newCity로 변경되어 있음..!!
-            // 임베디드 타입을 공유하면 이렇게 사이드 이펙트가 생김!!
+            // 값 타입의 부작용 -> 수정 불가능하도록하기(불변 객체)
             member.getHomeAddress().setCity("newCity");
 
 
